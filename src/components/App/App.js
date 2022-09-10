d59641736e12565a5eebd7fd69ba5fa4224d3f73
Sasha <aleks-one@ukr.net>
2022-09-10 12:29:17 +0300

    Implement outlet from react-router-dom

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,14 +3,13 @@ import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import Header from '../Header/Header';
 import MainPage from '../MainPage/MainPage';
 import Deco from '../Deco/Deco';
 import About from '../About/About';
 import Furniture from '../Furniture/Furniture';
 import Cart from '../Cart/Cart';
-import Footer from '../Footer/Footer';
 import ProductDetails from '../ProductDetails/ProductDetails';
+import Layout from '../Layout/Layout';
 
 const darkTheme = createTheme({
   typography: {
@@ -56,17 +55,17 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={darkTheme}>
         <CssBaseline />
-          <Header />
           <Routes>
-            <Route path='/' element={<MainPage/>}/>
-            <Route path='/home-decoration' element={<Deco/>}/>
-            <Route path='/furniture' element={<Furniture/>}/>
-            <Route path='/about' element={<About/>}/>
-            <Route path='/cart' element={<Cart/>}/>
-            <Route path='/furniture/:id' element={<ProductDetails category={'furniture'}/>}/>
-            <Route path='/home-decoration/:id' element={<ProductDetails category={'home-decoration'}/>}/>
+            <Route path='/' element={<Layout/>}>
+              <Route index element={<MainPage/>}/>
+              <Route path='home-decoration' element={<Deco/>}/>
+              <Route path='furniture' element={<Furniture/>}/>
+              <Route path='about' element={<About/>}/>
+              <Route path='cart' element={<Cart/>}/>
+              <Route path='furniture/:id' element={<ProductDetails category={'furniture'}/>}/>
+              <Route path='home-decoration/:id' element={<ProductDetails category={'home-decoration'}/>}/>
+            </Route>
           </Routes>
-          <Footer />
       </ThemeProvider>
     </QueryClientProvider>
   );
